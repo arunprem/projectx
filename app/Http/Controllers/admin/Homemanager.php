f22cadeb62cef5654c38f870f3d3f5612eaebba7
Arun <arun.612675@kerala.gov.in>
2022-10-21 22:01:34 +0530

    image intervention package installed

--- app/Http/Controllers/admin/Homemanager.php
+++ app/Http/Controllers/admin/Homemanager.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\HomeSlide;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
 
 class Homemanager extends Controller
 {
@@ -13,19 +15,18 @@ class Homemanager extends Controller
     public function viewHomeSection()
     {
         $homeSection = HomeSlide::find(1);
-        return view('admin.page.home_section_admin_view',compact('homeSection'));
+        return view('admin.page.home_section_admin_view', compact('homeSection'));
     }
 
-    public function saveHomeSlider(Request $request){
-     
-        $slide_id = $request->id;
-        if($request->file('slider_image')){
-            $image = $request->file('slider_image');
-            $fileName = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-            
-        //    Image::make($image)->resize();
-            
+    public function saveHomeSlider(Request $request)
+    {
 
+        $slide_id = $request->id;
+        if ($request->file('slider_image')) {
+            $image = $request->file('slider_image');
+            $fileName = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+
+            Image::make($image)->resize(636, 852);
         }
     }
 
