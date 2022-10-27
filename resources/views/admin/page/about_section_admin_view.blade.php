220992b87e7650aeaadea9925a3011ed6fb70e4d
Arun <arun.612675@kerala.gov.in>
2022-10-27 17:15:04 +0530

    mulitpile image upload started

--- resources/views/admin/page/about_section_admin_view.blade.php
+++ resources/views/admin/page/about_section_admin_view.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <div class="page-content">
         <div class="container-fluid">
             <div class="row">
@@ -30,14 +31,14 @@
                                 <input name="id" type="hidden" value="{{ $aboutSection->id }}" />
                                 <div class="row mb-3">
                                     <label for="title-input" class="col-sm-2 col-form-label">Title</label>
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-4">
                                         <input class="form-control" name="title" type="text"
                                             value="{{ $aboutSection->title_about }}" placeholder="title" id="title-input">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <label for="title-input" class="col-sm-2 col-form-label">Short Description</label>
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-6">
                                         <input class="form-control" name="short_description" type="text"
                                             value="{{ $aboutSection->short_description }}" placeholder="Description"
                                             id="title-input">
@@ -45,7 +46,7 @@
                                 </div>
                                 <div class="row mb-3">
                                     <label for="title-input" class="col-sm-2 col-form-label">Description</label>
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-8">
                                         <textarea class="form-control" id="description" name="long_description" rows="3" cols="25"> {{ $aboutSection->long_description }}
                                             </textarea>
                                     </div>
@@ -53,17 +54,17 @@
 
 
                                 <div class="row mb-3">
-                                    <label for="sliderImage-input" class="col-sm-2 col-form-label">About us
-                                        Image</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" name="slider_image" type="file" value=""
-                                            id="sliderImage-input">
+                                    <label for="aboutImage-input" class="col-sm-2 col-form-label">About us
+                                        Images</label>
+                                    <div class="col-sm-4">
+                                        <input class="form-control" name="about_image" type="file" value=""
+                                            id="aboutImage-input">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="sliderImage-input" class="col-sm-2 col-form-label"></label>
+                                    <label for="about-input" class="col-sm-2 col-form-label"></label>
                                     <div class="col-sm-10">
-                                        <img id="showImage" class="rounded avatar-lg" src="" alt="Card image cap">
+                                        <img id="showImage" class="rounded avatar-lg" src="{{ !empty($aboutSection->about_iamge) ? url($aboutSection->about_image) : url('images/no_image.jpg') }}" alt="Card image cap">
                                     </div>
                                 </div>
                                 <input type="submit" value="Update About us"
@@ -78,4 +79,16 @@
 
         </div>
     </div>
+
+    <script>
+        $(document).ready(function() {
+            $('#aboutImage-input').change(function(e) {
+                var imageReader = new FileReader();
+                imageReader.onload = function(e) {
+                    $('#showImage').attr('src', e.target.result);
+                }
+                imageReader.readAsDataURL(e.target.files['0']);
+            })
+        });
+    </script> 
 @endsection
